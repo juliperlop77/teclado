c5094195bb630073f2d5670ef2e620b1f34dd2db
JUANA LIDIA PEREZ LOPEZ <juliperlop77@hotmail.com>
2014-08-19 14:20:39 -0500

    dos
    segunda subida

--- js/acciones.js
+++ js/acciones.js
@@ -13,7 +13,7 @@ $(document).ready(function(e){
 	audio.preloadFX('la','audio/LA.mp3', function(){},function (e) {alert('Error '+e);});
 	audio.preloadFX('si','audio/SI.mp3', function(){},function (e) {alert('Error '+e);});
 	$('.nota').bind('touchstart',function(){$(this).addClass('tocada');
-	audio.play($(this).attr('id'));})
+	audio.play($(this).attr('id'));});
 	$('.nota').bind('touchend',function(){ $(this).removeClass ('tocada');
 	});
 	},false); //deviceready
