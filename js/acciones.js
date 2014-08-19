6ecb07721ce241b81c806960aef544c2d70bba49
JUANA LIDIA PEREZ LOPEZ <juliperlop77@hotmail.com>
2014-08-19 14:01:50 -0500

    uno
    primera subida

---
+++ js/acciones.js
@@ -0,0 +1,20 @@
+//JavaScript
+// Todo lo que se ponga después de ésta línea de código se va a ejecutar después de que esté listo el documento.	
+$(document).ready(function(e){	
+	 $('#principal').height($('#page').height());
+	document.addEventListener("deviceready",function(){
+// Todo lo que se ponga después de ésta línea de código se va a ejecutar después de que esté listo el dispositivo esté listo.
+    audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('do','audio/DO.mp3', function(){},function (e) {alert('Error '+e);});
+	audio.preloadFX('re','audio/RE.mp3', function(){},function (e) {alert('Error '+e);});
+	audio.preloadFX('mi','audio/MI.mp3', function(){},function (e) {alert('Error '+e);});
+	audio.preloadFX('fa','audio/FA.mp3', function(){},function (e) {alert('Error '+e);});
+	audio.preloadFX('sol','audio/SOL.mp3', function(){},function (e) {alert('Error '+e);});
+	audio.preloadFX('la','audio/LA.mp3', function(){},function (e) {alert('Error '+e);});
+	audio.preloadFX('si','audio/SI.mp3', function(){},function (e) {alert('Error '+e);});
+	$('.nota').bind('touchstart',function(){$(this).addClass('tocada');
+	audio.play($(this).attr('id'));})
+	$('.nota').bind('touchend',function(){ $(this).removeClass ('tocada');
+	});
+	},false); //deviceready
+});//ready
